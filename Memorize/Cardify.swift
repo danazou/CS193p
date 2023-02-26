7b187edcf0f4fc63545c898b25e11cd677604860
danazou <52993274+danazou@users.noreply.github.com>
2023-02-26 18:16:59 +0000

    add Cardify struct

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,14 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Dana Zou on 26/02/2023.
+//
+
+import SwiftUI
+
+//struct Cardify: ViewModifier {
+//    func body(content: Content) -> some View {
+//        <#code#>
+//    }
+//}
